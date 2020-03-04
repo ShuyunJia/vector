fce0cecce8065138891ce538bfee0e8860543b21
shuyun <shuyunjia@outlook.com>
2020-03-04 11:49:29 +0800

    update sample code
    Signed-off-by: shuyun <shuyunjia@outlook.com>

--- src/vec-vcmul/vec_vcmul_main.c
+++ src/vec-vcmul/vec_vcmul_main.c
@@ -21,9 +21,9 @@ float input_data_Y[DATA_SIZE] = {561, 209, 74, 637, 959, 469, 585, 70};
 
 int main( int argc, char* argv[] )
 {
-  uint32_t input_data[4] = {0x00040008,0x00140018,0xfffeffd0,0x11221122};
-  uint32_t output_data[4] = {0, 0, 0, 0};
-  uint32_t verify_data[4] = {0xffd00040,0xff5003c0,0xf70400c0,0x00001108}; 
+  uint64_t input_data[4] = {0x0000000400000008,0x0000001400000018,0xfffffffeffffffd0,0x0000112200001122};
+  uint64_t output_data[4] = {0, 0, 0, 0};
+  uint64_t verify_data[4] = {0xffffffd000000040,0xffffff50000003c0,0xfffff704000000c0,0x0000000000001108}; 
   // Do the saxpy
   setStats(1);
   vec_vvadd_asm(input_data, output_data);
--- src/vec-vvadd/vec_vvadd_main.c
+++ src/vec-vvadd/vec_vvadd_main.c
@@ -12,22 +12,29 @@
 // Input/Reference Data
 
 //#include "dataset1.h"
-#define DATA_SIZE 8
-float input_data_X[DATA_SIZE] = {561, 209, 74, 637, 959, 469, 585, 70};
-float input_data_Y[DATA_SIZE] = {561, 209, 74, 637, 959, 469, 585, 70};
-
+#define DATA_SIZE 4 
 //--------------------------------------------------------------------------
 // Main
 
 int main( int argc, char* argv[] )
 {
-  float result[DATA_SIZE];
+  int input_data_X[DATA_SIZE] ;
+  int input_data_Y[DATA_SIZE];
+  int result[DATA_SIZE];
   // Do the saxpy
-  //setStats(1);
+  for(int i =0; i<DATA_SIZE ; i++){
+    input_data_X[i] = -(i + 0x100);
+    input_data_Y[i] = 0x101;
+  }
+  setStats(1);
   vec_vvadd_asm(DATA_SIZE, result, input_data_X, input_data_Y);
   setStats(0);
+  printf("data   = %d %d %d %d\n", input_data_X[0], input_data_X[1], input_data_X[2], input_data_X[3]);
+  printf("result = %d %d %d %d\n", result[0], result[1], result[2], result[3]);
+  printf("hello wolrd test\n");
 
   // Check the results
   //return verifyFloat(DATA_SIZE, result, verify_data);
+  //while(1);
   return 0;
 }
